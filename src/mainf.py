29feafc4767323e1eaa99db2c25aeb9e2019c4b3
fereshteh1989 <fereshteh.a@gmail.com>
2019-07-08 08:51:56 -0700

    Print message added
    Print message added, stating why I'm reading back the report.csv file and printing the output.

--- src/mainf.py
+++ src/mainf.py
@@ -41,6 +41,7 @@ class Report:
                     reporter.writerow([('%d')%(self.departments[ind].ID)]+[('%d')%(self.departments[ind].totalOrder+self.departments[ind].totalReorder)]+[('%d')%(self.departments[ind].totalOrder)]+[('%s')%(round(self.departments[ind].totalOrder/(self.departments[ind].totalOrder+self.departments[ind].totalReorder),2))])
                 else:
                     continue
+        print('Since the web-based test environment does not seem to "see" my output file, Im reading the info back from the report.csv file and printing them!')
         with open(path3, encoding="utf8") as csvfile:
             tester = csv.reader(csvfile,delimiter =',')
             for row in tester:
